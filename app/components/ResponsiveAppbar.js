6c1bf93e4e439429eaef4ee677944c101d9f18d2
j3rryl <jeremymunroe0@gmail.com>
2023-05-11 18:55:13 +0300

    alvocat start

---
+++ app/components/ResponsiveAppbar.js
@@ -0,0 +1,63 @@
+"use client"; // this is a client component
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import AdbIcon from '@mui/icons-material/Adb';
+import { APP_NAME, PAGES_1, PAGES_2 } from '../utils/constants';
+
+function ResponsiveAppBar() {
+  return (
+    <AppBar position="static" sx={{
+      backgroundColor: "#63d471",
+      backgroundImage: "linear-gradient(315deg, #63d471 0%, #233329 74%)"
+    }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            {APP_NAME}
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {PAGES_1.map((page) => (
+              <Button
+                key={page}
+                sx={{ my: 2, color: 'white', display: 'block', textTransform:"capitalize" }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box> 
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' },justifyContent:"end" }}>
+            {PAGES_2.map((page) => (
+              <Button
+                key={page}
+                sx={{ my: 2, color: 'white', display: 'block', textTransform:"capitalize" }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>         
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default ResponsiveAppBar;
---
+++ app/utils/constants.js
@@ -0,0 +1,4 @@
+export const APP_NAME= "Alvocat"
+export const PAGES_1 = ['Home', 'Products', 'Pricing'];
+export const PAGES_2 = ["Socials",  'Blog', "Contact Us"];
+
