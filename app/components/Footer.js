37635a4244c5af3f19b72385fd1ede9196d10122
j3rryl <jeremymunroe0@gmail.com>
2023-05-13 13:42:36 +0300

    footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -66,10 +66,8 @@ export const Footer = () => {
               </a>
               <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
               <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-  <path
-    d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
-</svg>
-                  <span className="sr-only">LinkedIn</span>
+                    <path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" /></svg>
+                    <span className="sr-only">LinkedIn</span>
               </a>
           </div>
       </div>
