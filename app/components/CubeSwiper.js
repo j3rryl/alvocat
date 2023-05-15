12b2b0578eea4e967c7ea86cbc6df04b8246e11f
j3rryl <jeremymunroe0@gmail.com>
2023-05-15 16:37:16 +0300

    img change

--- app/components/CubeSwiper.js
+++ app/components/CubeSwiper.js
@@ -9,6 +9,7 @@ import "swiper/css/pagination";
 
 // import required modules
 import { Autoplay, EffectCube, Pagination } from "swiper";
+import Image from "next/image";
 
 export default function CubeSwiper({imageUrls}) {
   return (
@@ -34,7 +35,7 @@ export default function CubeSwiper({imageUrls}) {
         {imageUrls?.map((item)=>{
           return (
           <SwiperSlide key={item}>
-          <img src={item}/>
+          <Image src={item}/>
           </SwiperSlide>
           )
         })}
